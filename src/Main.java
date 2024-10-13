a3f8b9232b364003c9e6595109a63e8dec5f9349
Egurazdov <egurazdov@yandex.ru>
2024-10-13 13:50:53 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+        changeFirstAndLast(list);
+        System.out.println(list);
+    }
+    private static void changeFirstAndLast(List<Integer> list){
+        int f = list.getFirst();
+        int l = list.getLast();
+        list.removeLast();
+        list.removeFirst();
+        list.addFirst(l);
+        list.addLast(f);
     }
 }
